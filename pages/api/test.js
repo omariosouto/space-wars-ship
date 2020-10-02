e89fa04fdafccd604e2582c85359e20e2d26b6c8
Mario Souto <soutomarios@gmail.com>
2020-10-01 21:44:00 -0300

    add test

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,90 +1,33 @@
-import { launch } from 'puppeteer-core';
-import chrome from 'chrome-aws-lambda';
+import chromium from 'chrome-aws-lambda';
 
-const exePath = process.platform === 'win32'
-? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-: process.platform === 'linux'
-? '/usr/bin/google-chrome'
-: 'Applications/Chromium.app/Contents/MacOS/Chromium';
-// : '/Applications/Chromium.app/Contents/MacOS/Chromium'
+export default async (req,res) => {
+    const options = {
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+    };
+    const browser = await chromium.puppeteer.launch(options);
+    
+    const page = await browser.newPage();
 
-export async function getOptions(isDev) {
-  let options
-  if (isDev) {
-      options = {
-          args: [],
-          executablePath: exePath,
-          headless: true
+    await page.goto('https://example.com');
+
+    const dimensions = await page.evaluate(() => {
+      return {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        deviceScaleFactor: window.devicePixelRatio
       };
-  } else {
-      options = {
-          args: chrome.args,
-          executablePath: await chrome.executablePath,
-          headless: chrome.headless,
-      };
-  }
-  console.log('[[[[[[[[[options]]]]]]]]]', options, '[isDev]', isDev);
-  return options;
-}
-
-/// =====================
-/// =====================
-
-let _page;
-async function getPage(isDev) {
-  if (_page) {
-    return _page;
-  }
-  const options = await getOptions(isDev);
-  const browser = await launch(options);
-  _page = await browser.newPage();
-  return _page;
-}
-
-async function getScreenshot(html, isDev) {
-  const page = await getPage(isDev);
-  await page.setViewport({ width: 2048, height: 1170 });
-  await page.setContent(html);
-  const file = await page.screenshot();
-  return file;
-}
-
-/// =====================
-/// =====================
-
-function getHTML() {
-  return `
-    <p>Vrau!</p>
-  `;
-}
-
-const isDev = !process.env.AWS_REGION;
-process.env.OG_HTML_DEBUG = '0';
-const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
-
-/// =====================
-/// =====================
-
-export default async function handler(req, res) {
-  try {
-
-    const html = getHTML();
-
-    if (isHtmlDebug) {
-        res.setHeader('Content-Type', 'text/html');
-        res.end(html);
-        return;
-    }
-
-    const response = await getScreenshot(html, isDev);
-
+    });
+  
+    console.log('Dimensions:', dimensions);
+  
+    await browser.close();
+  
     res.send({
-      response
+        statusCode: 200,
+        response: dimensions,
     })
-  } catch (e) {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
-    console.error(e);
-  }
+
 }
